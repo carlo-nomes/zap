d970c8086eb754e70105cc16befd972c1775a0d0
carlo.nomes <nomes.carlo@gmail.com>
2021-10-03 13:35:32 +0200

    :building_construction: add TS and rename files

--- src/common/emojis/index.js
+++
@@ -1,6 +0,0 @@
-import HearNoEvil from "./collection/HearNoEvil";
-import SeeNoEvil from "./collection/SeeNoEvil";
-import ConstructionSign from "./collection/ConstructionSign";
-import Wave from "./collection/Wave";
-
-export { HearNoEvil, SeeNoEvil, ConstructionSign, Wave };
---
+++ src/common/emojis/index.ts
@@ -0,0 +1,6 @@
+import HearNoEvil from "./HearNoEvil";
+import SeeNoEvil from "./SeeNoEvil";
+import ConstructionSign from "./ConstructionSign";
+import Wave from "./Wave";
+
+export { HearNoEvil, SeeNoEvil, ConstructionSign, Wave };
