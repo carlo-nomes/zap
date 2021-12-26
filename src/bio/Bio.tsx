b99272f23229d108ccb77164c6bd18b58efeef7a
carlo.nomes <nomes.carlo@gmail.com>
2021-12-26 16:27:28 +0100

    update bio content

--- src/bio/Bio.tsx
+++ src/bio/Bio.tsx
@@ -60,17 +60,17 @@ const Bio = () => {
         <br />
         my name is Carlo.
         <br />
-        <br />I am a full-stack JavaScript developer and UX/UI enthusiast and have been using my passion for technology
-        to help organizations create amazing web applications since 2017.
+        <br />I am a full-stack JavaScript developer and UX/UI enthusiast. <br />
+        Since 2017 I have been using my passion for technology to help organizations create amazing web applications.
         <br />
         <br />
         My credo is:
         <br />
-        <q>For every problem, there exists an elegant solution.</q>
-        <br />I get to these solutions by using the latest and greatest tools/techniques available. Through persistent
-        learning and critical reflection, I maintain a current view of what is needed to make a project succeed.
-        <br />I believe that every successful project requires agility and an open, honest and clear line of
-        communication between all stakeholders and team members, regardless of the size of the project.
+        <q>For every challenge, there exists an elegant solution.</q>
+        <br />I get these solutions with the latest and greatest tools/techniques available. Through persistent learning
+        and critical reflection, I maintain a current view of what is needed to make any project succeed.
+        <br />I believe that every successful endeavor, no matter the size or complexity, requires agility and an open,
+        honest and clear line of communication between all parties.
       </Text>
       <Spacer />
       <Arrow href="#timeline" />
